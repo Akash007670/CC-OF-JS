b485785325b16c1d310047b9eca03e27afd4d51e
Akash007670 <akaysharma894@gmail.com>
2021-04-07 23:08:16 +0530

    Added anonyomous function and first class function

--- Namaste Javascript/FirstClassFunction.js
+++ Namaste Javascript/FirstClassFunction.js
@@ -14,3 +14,26 @@ var b = function () {
   console.log("b called");
 };
 b();
+
+//Anonyomous Function
+//An anonymous function is a function without a name. An anonymous function is often not accessible after its initial creation
+// function () {
+
+// }
+
+//Named Function Expression
+//This is same as Function Expression but not with anonyomous Function
+var c = function xyz() {
+  console.log("c called");
+};
+c();
+
+//Difference b/w Parameters & Arguments
+
+var d = function (param1, param2) {
+  console.log(param1, param2);
+};
+d(1, 2); //here 1 ,2 are arguments
+
+//First class Function
+// A function can be passed as an argument to other functions, can be returned by another function and can be assigned as a value to a variable.
