1844523cc660ca22fd7e1fc52aef34c4c8c72fc6
Akash007670 <akaysharma894@gmail.com>
2021-04-08 23:29:23 +0530

    Added EventListener with example of closures

--- Namaste Javascript/Callback.js
+++ Namaste Javascript/Callback.js
@@ -15,4 +15,20 @@ x(function y() {
 //JavaScript is a synchronous and single-threaded language.
 
 //Blocking the main thread
+//Here debug the line4 and line 12.
+
 //Power of Callbacks?
+
+//Deep about Event listeners.
+
+function attachEventListener() {
+  let count = 0;
+  document.getElementById("clickMe").addEventListener("click", function xyz() {
+    console.log("Hello I am called.", count++);
+  });
+}
+attachEventListener();
+
+//In above attachEventListener we did closure with EventListner where attachEventListener is just a name of a function.
+
+//Garbage Collection and removeEventListeners
