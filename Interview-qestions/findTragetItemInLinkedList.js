b66eb0cb2762d48681ad68f25615e742af42a9a6
Akash kumar <akaysharma894@gmail.com>
2023-05-19 12:54:00 +0530

    Added getvalue from specific index in linked list.

--- Interview-qestions/findTragetItemInLinkedList.js
+++ Interview-qestions/findTragetItemInLinkedList.js
@@ -32,3 +32,17 @@ function findLinkList(head, target) {
 let res = findLinkList(a, 10)
 console.log(res) // returns true because 10 is there in linked list.
 
+function getValueFromSpecificIndex (head, index) {
+  let current = head;
+  let count = 0;
+  while(current !== null) {
+    if (count === index) return current.val
+    count += 1
+    current = current.next
+  }
+  return null;
+}
+
+let resp = getValueFromSpecificIndex(a, 2) //Here a is the head and 2 is the index i want to find the value.
+console.log(resp) //returns 30 
+
